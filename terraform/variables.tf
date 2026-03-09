689165a70274276aa37376cffa121ffb37633eb1
rajesh <rajeshgovindan777@gmail.com>
2026-03-09 16:05:43 +0000

    updated vpc code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,28 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+variable "project" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "micro-dash"
+}
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
